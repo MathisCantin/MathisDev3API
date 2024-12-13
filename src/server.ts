2b01526356d9c0fd4ec51d20537fb5e54d1dc610
MathisCantin <2240246@etudiant.cegepvicto.ca>
2024-12-13 16:26:01 -0500

    Fix 7

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,8 @@ import path from 'path';
 import helmet from 'helmet';
 import express, { Request, Response, NextFunction } from 'express';
 import logger from 'jet-logger';
-
 import 'express-async-errors';
-
 import BaseRouter from '@src/routes';
-
 import Paths from '@src/common/Paths';
 import EnvVars from '@src/common/EnvVars';
 import HttpStatusCodes from '@src/common/HttpStatusCodes';
@@ -22,8 +19,8 @@ const app = express();
 // **** Setup **** //
 
 // Basic middleware
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json());  // Body parser for JSON
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(EnvVars.CookieProps.Secret));
 
 // Show routes called in console during development
@@ -31,18 +28,19 @@ if (EnvVars.NodeEnv === NodeEnvs.Dev.valueOf()) {
   app.use(morgan('dev'));
 }
 
-// Security
+// Security middleware
 if (EnvVars.NodeEnv === NodeEnvs.Production.valueOf()) {
   app.use(helmet());
 }
 
+// CORS (Cross-Origin Resource Sharing) setup
 app.use(cors({
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-// Add APIs, must be after middleware
+// Add routes, must be after middleware
 app.use(Paths.Base, BaseRouter);
 
 // Add error handler
@@ -58,20 +56,27 @@ app.use((err: Error, _: Request, res: Response, next: NextFunction) => {
   return next(err);
 });
 
-
 // **** Front-End Content **** //
 
-// Set views directory (html)
+// Set views directory (HTML)
 const viewsDir = path.join(__dirname, 'views');
 app.set('views', viewsDir);
 
-// Set static directory (js and css).
+// Set static directory (JS, CSS files)
 const staticDir = path.join(__dirname, 'public');
 app.use(express.static(staticDir));
 
-// Nav pg by default
+// Redirect to API docs page by default
 app.get('/', (_: Request, res: Response) => {
   return res.redirect('api/docs');
+});
+
+// **** Start the Server **** //
+
+// Use the environment variable for port or fallback to 3000
+const port = EnvVars.Port || 3000;
+app.listen(port, () => {
+  logger.info(`Express server started on port: ${port}`);
 });
 
 // **** Export default **** //
